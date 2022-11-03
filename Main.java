1a6d4038fd1a11d6ca3c0bac0673828cd05a08d6
RachelRebecca <rachelrebecca67@gmail.com>
2022-11-03 17:57:14 -0400

    added tab with nimbus look and feel

---
+++ Main.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame
+{
+    public Main()
+    {
+        setTitle("Finance Project");
+        setSize(1000, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        setResizable(true);
+
+        JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.setForeground(Color.BLACK);
+
+        // add all the tabs to the Main frame's JTabbedPane
+        tabbedPane.add("Play in the Sandbox", new Sandbox());
+        tabbedPane.add("Do Actual Finance Stuff", new Finance());
+        tabbedPane.setPreferredSize(new Dimension(950, 550));
+
+        add(tabbedPane);
+    }
+
+    public static void main(String[] args)
+    {
+        Font font = new Font("Lucida Sans Unicode", Font.PLAIN, 12);
+
+        // update the UIManager to use the Nimbus Look and Feel
+        try
+        {
+            UIManager.LookAndFeelInfo[] lookAndFeels = UIManager.getInstalledLookAndFeels();
+            for (UIManager.LookAndFeelInfo info : lookAndFeels)
+            {
+                if ("Nimbus".equals(info.getName()))
+                {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ignored) {}
+
+        // change the font of the program
+        UIManager.put("Button.font", font);
+        UIManager.put("Label.font", font);
+        UIManager.put("TabbedPane.font", font);
+        UIManager.put("TextField.font", font);
+        UIManager.put("OptionPane.messageFont", font);
+        UIManager.put("TextArea.font", font);
+        UIManager.put("ComboBox.font", font);
+
+        // instantiate the Main frame
+        JFrame frame = new Main();
+        frame.setVisible(true);
+    }
+}
