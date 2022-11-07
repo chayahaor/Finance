bf5cd888db8640034495615d76ca982e140854be
RachelBrisman <rachaelibrisman@gmail.com>
2022-11-07 21:04:11 +0200

    got a tiny start

--- src/main/java/finance/Finance.java
+++ src/main/java/finance/Finance.java
@@ -1,8 +1,25 @@
 package finance;
 
+import sandbox.DatePanel;
+
 import javax.swing.*;
 
 public class Finance extends JPanel
 {
+    private JTextArea currentValue;
+    private JComboBox<String> action;
+    private JComboBox<String> initialCurrency;
+    private JComboBox<String> toCurrency;
+    private JFormattedTextField amount;
+    private JFormattedTextField fxRate;
+    private DatePanel maturityDate;
+    private JButton doAction;
 
+    public Finance()
+    {
+        setSize(900, 500);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+
+    }
 }
