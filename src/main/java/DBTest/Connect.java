17c33dd6820ff5cb0a7bc6e71c64ffa370cbf7de
RachelRebecca <rachelrebecca67@gmail.com>
2022-11-22 19:50:01 -0500

    added temporary database - to use must convert file to .sql and import into mysql

--- src/main/java/DBTest/Connect.java
+++ src/main/java/DBTest/Connect.java
@@ -5,17 +5,16 @@ import java.sql.*;
 public class Connect {
 
     public static void main(String[] args) {
-        //TODO: When using this code, make sure to change the statement query
         String dbName = "finance";
         try
         {
             Connection connection = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/"+dbName, "root", "");
             Statement stmt = connection.createStatement();
-            ResultSet resultSet = stmt.executeQuery("Select * from dbTableName");
+            ResultSet resultSet = stmt.executeQuery("Select * from action");
             while (resultSet.next())
             {
-                System.out.println(resultSet.getString(1));
+                System.out.println(resultSet.getString(2));
             }
         }
         catch (Exception e)
